83611369adbe1b74a0b1307f75a83883a6c05722
Techno Bug Hunting <87409181+techno241@users.noreply.github.com>
2022-04-11 02:25:47 +0800

    Update xtls.sh

--- xtls.sh
+++ xtls.sh
@@ -1,118 +1,51 @@
 
-cat << EOF > /etc/xray-mini/vless-splice.json
+cat << EOF > /usr/local/etc/xray/config.json
 {
     "log": {
         "access": "/var/log/xray/access.log",
         "error": "/var/log/xray/error.log",
-        "loglevel": "info"
-  },
-  "inbounds": [
-    {
-      "port": 443,
-      "protocol": "vless",
-      "settings": {
-        "clients": [
-          {
-            "id": "c3d59878-b02f-49cc-8c97-f7b8af2cfe98",
-            "flow": "xtls-rprx-direct",
-            "level": 0
-#XRay
-          }
-        ],
-        "decryption": "none",
-        "fallbacks": [
-          {
-            "dest": 80
-          },
-          {
-            "alpn": "h2",
-            "dest": 2001,
-            "xver": 0
-          }
-        ]
-      },
-      "streamSettings": {
-        "network": "tcp",
-        "security": "xtls",
-        "xtlsSettings": {
-          "alpn": [
-            "h2",
-            "http/1.1"
-          ],
-          "certificates": [
-            {
-              "certificateFile": "/etc/v2ray/v2ray.crt",
-              "keyFile": "/etc/v2ray/v2ray.key"
-            }
-          ]
-        }
-      }
+        "loglevel": "warning"
     },
-    {
-      "port": 2001,
-      "listen": "127.0.0.1",
-      "protocol": "vless",
-      "settings": {
-        "clients": [
-          {
-            "id": "c3d59878-b02f-49cc-8c97-f7b8af2cfe98"
-          }
-        ],
-        "decryption": "none"
-      },
-      "streamSettings": {
-        "network": "grpc",
-        "grpcSettings": {
-          "serviceName": "grpc"
-        }
-      }
-    },
-    {
-      "port": 80,
-      "protocol": "vmess",
-      "settings": {
-        "clients": [
-          {
-            "id": "c3d59878-b02f-49cc-8c97-f7b8af2cfe98"
-          }
-        ]
-      },
-      "streamSettings": {
-        "network": "tcp",
-        "tcpSettings": {
-          "header": {
-            "type": "http",
-            "response": {
-              "version": "1.1",
-              "status": "200",
-              "reason": "OK",
-              "headers": {
-                "Content-Type": [
-                  "application/octet-stream",
-                  "video/mpeg",
-                  "application/x-msdownload",
-                  "text/html",
-                  "application/x-shockwave-flash"
+    "inbounds": [
+        {
+            "port": 443,
+            "protocol": "vless",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "5d8077d7-9b3e-4025-b182-c4e2c4e37a09",
+                        "flow": "xtls-rprx-direct",
+                        "email": "dani@bugtop.xray.gkvm.my"
+                    }
                 ],
-                "Transfer-Encoding": [
-                  "chunked"
-                ],
-                "Connection": [
-                  "keep-alive"
-                ],
-                "Pragma": "no-cache"
-              }
+                "decryption": "none",
+                "fallbacks": [
+                    {
+                        "dest": 80
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "tcp",
+                "security": "xtls",
+                "xtlsSettings": {
+                    "alpn": [
+                        "http/1.1"
+                    ],
+                    "certificates": [
+                        {
+                            "certificateFile": "/iriszz/xray/xray.crt",
+                            "keyFile": "/iriszz/xray/xray.key"
+                        }
+                    ]
+                }
             }
-          }
-        },
-        "security": "none"
-      }
-    }
-  ],
-  "outbounds": [
-    {
-      "protocol": "freedom"
-    }
-  ]
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom"
+        }
+    ]
 }
 EOF
